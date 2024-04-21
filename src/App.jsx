c0b2870becf605fba29042f6c6603c40b01914c3
OHshajim <ajshajimmax@gmail.com>
2024-04-22 03:55:50 +0600

    data post and show data with bangla style

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,48 @@ import './App.css'
 
 function App() {
   const [users, setUsers] = useState([])
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res=>res.json())
-    .then(data=>{
-      setUsers(data)
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data)
+      })
+  }, [])
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
     })
-  },[])
- const handleSubmit=e=>{
-  e.preventDefault();
-  const form = e.target;
-   const name = form.name.value ;
-  const email = form.email.value ;
-  const user = {name,email}
-  console.log(user);
- fetch('http://localhost:5000/users',{
-    method:"POST",
-    headers:{
-      'content-type' : 'application/json'
-    },
-    body : JSON.stringify(user)
-  })
-    .then(res=>res.json())
-    .then(data=>{
-      console.log('inside post response ' ,data);
-    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('inside post response ', data);
 
- }
+        const newUsers = [...users, data]
+        setUsers(newUsers)
+        form.reset();
+      })
+
+  }
   return (
     <>
       <h1>Users Management Client</h1>
       <p>Users : {users.length}</p>
       {
-        users.map(user=><p key={user.id}>{user.id}. {user.name} ---- {user.email} </p>)
+        users.map(user => <p key={user.id}>{user.id}. {user.name} ---- {user.email} </p>)
       }
       <br />
       <h2>New user</h2>
       <form onSubmit={handleSubmit}>
-      <label>Name</label> <br />
+        <label>Name</label> <br />
         <input type="text" name='name' placeholder='enter your name' /> <br />
         <label>Email</label> <br />
         <input type="email" name='email' placeholder='enter your email' /> <br />
